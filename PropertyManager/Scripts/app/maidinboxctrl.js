09d8019c4f1c1d6b10fbc795b3f17a4dc02e25e5
tuannghia248 <42398876+tuannghia248@users.noreply.github.com>
2019-01-04 02:14:51 +0700

    create maid inbox

---
+++ PropertyManager/Scripts/app/maidinboxctrl.js
@@ -0,0 +1,46 @@
+function MaidInboxCtrl($scope, $rootScope, $stateParams, $location, $timeout, xhrService, $anchorScroll, $filter) {
+
+$scope.loadMaidInbox = function() {
+       $scope.bigCurrentPage = $stateParams.page === undefined ? 1 : $stateParams.page;
+        $scope.filterData = {
+            "Page":$scope.bigCurrentPage,
+            "Limit":10,        
+        };
+        
+        xhrService.post("GetListMaidInbox",$scope.filterData)
+        .then(function(data) {
+                $scope.maidinbox=data.data.data;
+                $scope.totalItems = data.data.total;
+            },
+            function(error) {
+                console.log(error.statusText);
+            });
+        
+        
+    };
+
+    $scope.saveMaidInbox = function(){
+    	xhrService.post("SaveMaidInbox",$scope.maidinbox).then(function (data) {
+            swal("Thành công!", "", "success")
+            .then((value) => {
+                $('#employeeModal').modal('hide');
+                $scope.loadMaidInbox();
+            });
+        },
+        function (error) {
+            console.log(error.statusText);
+        });
+
+    };
+
+    $scope.pageChanged = function () {
+        $location.path("/maid/inbox")
+        .search({ page: $scope.bigCurrentPage, 
+                });
+    };
+
+
+
+}
+
+app.controller('MaidInboxCtrl', MaidInboxCtrl);
